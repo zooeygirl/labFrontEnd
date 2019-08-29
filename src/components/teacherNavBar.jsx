0bc944b5c59daa110bba3b9e6818556b44563075
zooeygirl <zooeythecalisi@gmail.com>
2019-08-29 22:19:26 +0200

    teacher list

--- src/components/teacherNavBar.jsx
+++ src/components/teacherNavBar.jsx
@@ -21,7 +21,7 @@ class TeacherNavBar extends Component {
                 className="nav-item nav-link"
                 to="/"
               >
-                {t.username}
+                {t.lastname}
               </Link>
             ))}
             <Link
